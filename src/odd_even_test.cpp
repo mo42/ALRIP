f94d43fedf7fc4a20fcd070d7317e36c2267d8f8
Moritz Winter <me@mo42.de>
2023-06-17 10:10:58 +0200

    Improve numerical stability of primitives

--- src/odd_even_test.cpp
+++ src/odd_even_test.cpp
@@ -1,35 +1,31 @@
 #include <vector>
+#include <cmath>
 
 #include "odd_even_test.h"
 #include "point.h"
 #include "vec.h"
 
 bool below(point& a, point& b, point& c) {
-  long m = (static_cast<long>(a.y) - static_cast<long>(b.y)) /
-           (static_cast<long>(a.x) - static_cast<long>(b.x));
+  long dx = static_cast<long>(a.x) - static_cast<long>(b.x);
+  long dy = static_cast<long>(a.y) - static_cast<long>(b.y);
+  if (std::abs(dx) <= 1) {
+    return static_cast<long>(c.x) < std::max(static_cast<long>(a.x), static_cast<long>(b.x));
+  }
+  long m = dy / dx;
   long d = static_cast<long>(a.y) - m * static_cast<long>(a.x);
   return static_cast<long>(c.y) < m * static_cast<long>(c.x) + d;
 }
 
-bool is_inside(point& p, std::vector<point>& polygon) {
+bool is_inside(const point& p, const std::vector<point>& polygon) {
   bool inside = false;
-  for (std::size_t i = 0, j = 1, k = 2, l = 3; i < polygon.size(); ++i,
-                   ++j %= polygon.size(), ++k %= polygon.size(),
-                   ++l %= polygon.size()) {
-    if (((p.x < polygon[i].x && p.x > polygon[j].x) ||
-         (p.x > polygon[i].x && p.x < polygon[j].x)) &&
-        (below(polygon[i], polygon[j], p)))
-      inside = !inside;
-    if ((p.x == polygon[j].x) &&
-        ((p.x < polygon[i].x && p.x > polygon[k].x) ||
-         (p.x > polygon[i].x && p.x < polygon[k].x)) &&
-        (p.y < polygon[j].y))
-      inside = !inside;
-    if ((p.x == polygon[j].x && p.x == polygon[k].x) &&
-        (!((p.x < polygon[i].x && p.x < polygon[l].x) ||
-           (p.x > polygon[i].x && p.x > polygon[l].x))))
+  std::size_t n = polygon.size();
+  
+  for (std::size_t i = 0, j = n - 1; i < n; j = i++) {
+    if (((polygon[i].y > p.y) != (polygon[j].y > p.y)) &&
+        (p.x < (polygon[j].x - polygon[i].x) * (p.y - polygon[i].y) / (polygon[j].y - polygon[i].y) + polygon[i].x))
       inside = !inside;
   }
+  
   return inside;
 }
 
