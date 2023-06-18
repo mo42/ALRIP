a12e80ac0da17f39f47585e01af047605f9da4ed
Moritz Knabben <me@mo42.de>
2023-06-18 22:37:30 +0200

    More geometric primitive simplifications

--- src/odd_even_test.cpp
+++ src/odd_even_test.cpp
@@ -1,63 +1,15 @@
 #include <vector>
-#include <cmath>
 
 #include "odd_even_test.h"
-#include "point.h"
 #include "vec.h"
 
-bool below(point& a, point& b, point& c) {
-  long dx = static_cast<long>(a.x) - static_cast<long>(b.x);
-  long dy = static_cast<long>(a.y) - static_cast<long>(b.y);
-  if (std::abs(dx) <= 1) {
-    return static_cast<long>(c.x) < std::max(static_cast<long>(a.x), static_cast<long>(b.x));
-  }
-  long m = dy / dx;
-  long d = static_cast<long>(a.y) - m * static_cast<long>(a.x);
-  return static_cast<long>(c.y) < m * static_cast<long>(c.x) + d;
-}
-
-bool is_inside(const point& p, const std::vector<point>& polygon) {
+bool is_inside(vec& p, std::vector<vec>& polygon) {
   bool inside = false;
   std::size_t n = polygon.size();
-  
   for (std::size_t i = 0, j = n - 1; i < n; j = i++) {
     if (((polygon[i].y > p.y) != (polygon[j].y > p.y)) &&
         (p.x < (polygon[j].x - polygon[i].x) * (p.y - polygon[i].y) / (polygon[j].y - polygon[i].y) + polygon[i].x))
       inside = !inside;
   }
-  
   return inside;
-}
-
-bool below(vec& a, vec& b, vec& c) {
-  double m = (a.y - b.y) / (a.x - b.x);
-  double d = a.y - m * a.x;
-  return c.y < m * c.x + d;
-}
-
-bool is_inside(vec& p, std::vector<vec>& polygon) {
-  bool inside = false;
-  for (std::size_t i = 0, j = 1, k = 2, l = 3; i < polygon.size(); ++i,
-                   ++j %= polygon.size(), ++k %= polygon.size(),
-                   ++l %= polygon.size()) {
-    if (((p.x < polygon[i].x && p.x > polygon[j].x) ||
-         (p.x > polygon[i].x && p.x < polygon[j].x)) &&
-        (below(polygon[i], polygon[j], p)))
-      inside = !inside;
-    if ((p.x == polygon[j].x) &&
-        ((p.x < polygon[i].x && p.x > polygon[k].x) ||
-         (p.x > polygon[i].x && p.x < polygon[k].x)) &&
-        (p.y < polygon[j].y))
-      inside = !inside;
-    if ((p.x == polygon[j].x && p.x == polygon[k].x) &&
-        (!((p.x < polygon[i].x && p.x < polygon[l].x) ||
-           (p.x > polygon[i].x && p.x > polygon[l].x))))
-      inside = !inside;
-  }
-  return inside;
-}
-
-bool is_inside(std::vector<point>& polygon, std::vector<point>& inside) {
-  point p = *inside.begin();
-  return is_inside(p, polygon);
 }
--- src/odd_even_test.h
+++ src/odd_even_test.h
@@ -4,34 +4,8 @@
 #include "vec.h"
 
 /**
- * Return true if the point c lies below the segment a b.
- * Point c lies between a and b.
- */
-bool below(point& a, point& b, point& c);
-
-/**
- * Return if the point lies inside the polygon.
- * There are at most two subsequent points p1, p2 such that p1.x ==
- * p2.x or p1.y == p2.y
- */
-bool is_inside(const point& p, const std::vector<point>& polygon);
-
-/**
- * Return if the point c lies below the segment a b.
- * Point c lies between a and b.
- */
-bool below(vec& a, vec& b, vec& c);
-
-/**
  * Return if the point lies inside the polygon.
  * There are at most two subsequent points p1, p2 such that p1.x ==
  * p2.x or p1.y == p2.y
  */
 bool is_inside(vec& p, std::vector<vec>& polygon);
-
-/**
- * Return true if the polygon inside lies inside polygon
- * polygon.
- * The polygons do not intersect.
- */
-bool is_inside(std::vector<point>& polygon, std::vector<point>& inside);
