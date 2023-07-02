22cbdc8cdd304a6023983b219205af9c686d5dab
Moritz Winter <me@mo42.de>
2023-07-02 20:24:12 +0200

    clang-format math file

--- src/math.cpp
+++ src/math.cpp
@@ -54,7 +54,9 @@ bool is_inside(vec& p, std::vector<vec>& polygon) {
   std::size_t n = polygon.size();
   for (std::size_t i = 0, j = n - 1; i < n; j = i++) {
     if (((polygon[i].y > p.y) != (polygon[j].y > p.y)) &&
-        (p.x < (polygon[j].x - polygon[i].x) * (p.y - polygon[i].y) / (polygon[j].y - polygon[i].y) + polygon[i].x))
+        (p.x < (polygon[j].x - polygon[i].x) * (p.y - polygon[i].y) /
+                       (polygon[j].y - polygon[i].y) +
+                   polygon[i].x))
       inside = !inside;
   }
   return inside;
