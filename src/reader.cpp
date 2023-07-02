24b34b4e113f3adbcb0774c0c1ebc3dab6f1e86b
Moritz Knabben <me@mo42.de>
2023-07-02 20:23:21 +0200

    Read GeoJSON files

--- src/reader.cpp
+++ src/reader.cpp
@@ -1,5 +1,7 @@
 #include <fstream>
+#include <iostream>
 #include <map>
+#include <rapidjson/document.h>
 #include <sstream>
 #include <string>
 #include <tuple>
@@ -19,6 +21,50 @@ static std::vector<std::string> split(const std::string& s, char d) {
   while (std::getline(stream, i, d))
     tokens.push_back(i);
   return tokens;
+}
+
+/**
+ * Read GeoJSON file and returns a vector of parsed_segments.
+ */
+inner_outer read_geo_json(std::string path) {
+  std::ifstream file(path);
+  std::string json_string((std::istreambuf_iterator<char>(file)),
+                          std::istreambuf_iterator<char>());
+  rapidjson::Document document;
+  document.Parse(json_string.c_str());
+  // Check if valid GeoJSON format
+  if (!document.IsObject() || !document.HasMember("type") ||
+      std::string(document["type"].GetString()) != "Polygon") {
+    std::cout << "Invalid GeoJSON format. Expecting Polygon." << std::endl;
+  }
+  const rapidjson::Value& polygons = document["coordinates"];
+  if (!polygons.IsArray()) {
+    std::cout << "No polygons found in the GeoJSON file." << std::endl;
+  }
+  std::vector<vec> outer;
+  std::vector<std::vector<vec>> inners;
+
+  for (rapidjson::SizeType i = 0; i < polygons.Size(); ++i) {
+    const rapidjson::Value& polygon = polygons[i];
+    if (!polygon.IsArray()) {
+      std::cout << "No polygon found in the GeoJSON file." << std::endl;
+    }
+    std::vector<vec> inner;
+    for (rapidjson::SizeType j = 0; j < polygon.Size(); ++j) {
+      const rapidjson::Value& coordinate = polygon[j];
+      vec v = vec(coordinate[0].GetDouble(), coordinate[1].GetDouble());
+      if (i == 0) {
+        outer.push_back(v);
+      } else {
+        inner.push_back(v);
+      }
+    }
+    if (inner.size() > 0) {
+      inners.push_back(inner);
+    }
+  }
+  std::vector<std::vector<vec>> outer_boxed = {outer};
+  return inner_outer(outer_boxed, inners);
 }
 
 /**
