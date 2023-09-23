a4b8bd99a8546a02a114061d06ef53bd14be8bbc
Moritz Winter <me@mo42.de>
2023-09-23 21:00:13 +0200

    Decommission obsolete function for obscure format

--- src/reader.h
+++ src/reader.h
@@ -9,8 +9,6 @@ typedef std::tuple<std::vector<std::vector<vec>>, std::vector<std::vector<vec>>>
     inner_outer;
 typedef std::vector<polygons> instances;
 
-inner_outer read_file(std::string path);
-
 /**
  * Parse polygons from a GeoJSON file.
  */
